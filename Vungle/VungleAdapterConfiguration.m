8aaef536701303c84544ebd3f4a87f168dc5c305
YueVungle <yueting.cui@vungle.com>
2020-03-17 21:41:20 +0800

    Follow MoPub code style guidelines

--- Vungle/VungleAdapterConfiguration.m
+++ Vungle/VungleAdapterConfiguration.m
@@ -30,7 +30,7 @@ typedef NS_ENUM(NSInteger, VungleAdapterErrorCode) {
 + (void)updateInitializationParameters:(NSDictionary *)parameters {
     // These should correspond to the required parameters checked in
     // `initializeNetworkWithConfiguration:complete:`
-    NSString * appId = parameters[kVungleAppIdKey];
+    NSString *appId = parameters[kVungleAppIdKey];
     
     if (appId != nil) {
         NSDictionary * configuration = @{ kVungleAppIdKey: appId };
@@ -59,7 +59,7 @@ typedef NS_ENUM(NSInteger, VungleAdapterErrorCode) {
 
 - (void)initializeNetworkWithConfiguration:(NSDictionary<NSString *, id> *)configuration
                                   complete:(void(^)(NSError *))complete {
-    NSString * appId = configuration[kVungleAppIdKey];
+    NSString *appId = configuration[kVungleAppIdKey];
     
     if (configuration[kVungleSDKCollectDevice]) {
         // Check if publisher has set this value and update Vungle here.
@@ -88,7 +88,7 @@ typedef NS_ENUM(NSInteger, VungleAdapterErrorCode) {
     }
     
     if (appId == nil) {
-        NSError * error = [NSError errorWithDomain:kAdapterErrorDomain code:VungleAdapterErrorCodeMissingAppId userInfo:@{ NSLocalizedDescriptionKey: @"Missing the appId parameter when configuring your network in the MoPub website." }];
+        NSError *error = [NSError errorWithDomain:kAdapterErrorDomain code:VungleAdapterErrorCodeMissingAppId userInfo:@{ NSLocalizedDescriptionKey: @"Missing the appId parameter when configuring your network in the MoPub website." }];
         MPLogEvent([MPLogEvent error:error message:nil]);
         
         if (complete != nil) {
